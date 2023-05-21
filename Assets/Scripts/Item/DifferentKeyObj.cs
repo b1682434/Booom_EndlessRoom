18703593c2dce8a289e08821221b683a36e7002b
b1682434 <B1682434@live.tees.ac.uk>
2023-05-21 16:40:06 +0100

    111

---
+++ Assets/Scripts/Item/DifferentKeyObj.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DifferentKeyObj : InteractableBase, IInteractRequest   
+{
+    public int KeyIDV1;
+    public int KeyIDV2;
+    bool opened;
+    public UnityEvent itemOpenV1;
+    public UnityEvent itemOpenV2;
+
+
+    public void InteractRequest(int ItemID)
+    {
+
+        if (opened)
+        {
+            showWord = openWord;
+        }
+        else
+        {
+            if (ItemID == KeyIDV1)//打开
+            {
+                opened = true;
+                showWord = openWord;
+                itemOpenV1.Invoke();
+            }else if (ItemID == KeyIDV2)
+            {
+                opened = true;
+                showWord = openWord;
+                itemOpenV2.Invoke();
+            }
+            else if (ItemID == 0)//空手
+            {
+                showWord = emptyHandWord; //应该可以继续优化。 需要钥匙的一个类，多种钥匙多种方法的一个子类，pickup一个
+            }
+            else // 拿了错的东西开门
+            {
+                showWord = cannotOpenWord;
+            }
+
+        }
+
+    }
+}

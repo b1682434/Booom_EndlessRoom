d3368ac91f36b4d39f7486c34eb7ef75b723a589
b1682434 <B1682434@live.tees.ac.uk>
2023-05-27 02:45:18 +0100

    物件能替换模型的都替换了，可互动物品做得差不多了。还搞了点碎裂动画

--- Assets/Scripts/Item/OpenItemsScripts/Tv.cs
+++ Assets/Scripts/Item/OpenItemsScripts/Tv.cs
@@ -7,18 +7,42 @@ public class Tv : MonoBehaviour
     public AudioSource au;
     public AudioClip noiseSound, rightSound;
     public Renderer screenRenderer;
-    public Material noiseMat, rightMat;
+    public Material noiseMat, rightMat, oriMat;
     public Clock clock;
+    bool inRightChannel;
+    bool open;
+
+    private void Start()
+    {
+        oriMat = screenRenderer.material;
+    }
     public void ToWrongChannel()
     {
+        inRightChannel = false;
         screenRenderer.material = noiseMat;
         au.clip = noiseSound;
         au.time = 0f;
         au.Play();
     }
 
+    public void OpenTV()
+    {
+        open = !open;
+        if (open)
+        {
+            if (inRightChannel) ToRightChannel();
+            else ToWrongChannel();
+        }
+        else
+        {
+            screenRenderer.material = oriMat;
+            au.Stop();
+        }
+    }
+
     public void ToRightChannel()
     {
+        inRightChannel = true;
         screenRenderer.material = rightMat;
         au.clip = rightSound;
         if(clock.minutes * 60f + clock.second< au.clip.length)

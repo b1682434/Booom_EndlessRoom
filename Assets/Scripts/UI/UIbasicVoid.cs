da7830405ff72b790cd5c5499a5bbb3b83f5a5b8
storyoftime <37776197+storyoftime66@users.noreply.github.com>
2023-05-28 22:33:59 +0800

    做了简易的标题画面场景

--- Assets/Scripts/UI/UIbasicVoid.cs
+++ Assets/Scripts/UI/UIbasicVoid.cs
@@ -2,23 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class UIbasicVoid : MonoBehaviour
 {
     // Start is called before the first frame update
     public void ReturnToMenu()
     {
-       
         SceneManager.LoadScene(0);
     }
 
-   
+
     public void LoadSelectLevel(int levelIndex)
     {
         SceneManager.LoadScene(levelIndex);
     }
 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("smallEyePlayground");
+    }
+
     public void Quit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
